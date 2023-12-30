bf01aa85f613c621f456f07621f93ed969709567
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-12-31 05:17:35 +0800

    Refactor scraper classes and add async functionality

--- newscraper.py
+++ newscraper.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from enum import Enum
 import httpx
+import asyncio
 import xml.etree.ElementTree as ET
 import datetime
 
@@ -24,20 +25,49 @@ class Provider(Enum):
 
 
 class ScraperStrategy(ABC):
+    @property
     @abstractmethod
+    def _category_mapping(self) -> dict:
+        pass
+
+    @property
+    @abstractmethod
+    def _rss_url(self) -> str:
+        pass
+
     async def scrape_all(self):
+        results = []
+        for category in self._category_mapping:
+            category_results = await self.scrape_category(category)
+            results.extend(category_results)
+        return results
+
+    async def scrape_category(self, category: Category) -> list:
+        if category in self._category_mapping:
+            mapped_category = self._category_mapping[category]
+            print(f"{self._cname()} scraping for {category} ({mapped_category})")
+
+            # Replace the [category] placeholder with the mapped category
+            rss_url = self._rss_url.replace("[category]", mapped_category)
+
+            # Fetch and parse the RSS feed asynchronously
+            rss_root = await self.fetch_rss(rss_url)
+
+            # Extract URLs from the RSS feed
+            urls = await self.parse_rss(rss_root, mapped_category)
+
+            # Scrape each URL asynchronously and return the results
+            tasks = [self.scrape_url(url) for url in urls]
+            return await asyncio.gather(*tasks)
+        else:
+            print(f"Category mapping not defined for {self._cname()}: {category}")
+
+    @abstractmethod
+    async def scrape_url(self, url: str) -> dict:
         pass
 
     @abstractmethod
-    async def scrape_category(self, category: Category):
-        pass
-
-    @abstractmethod
-    async def scrape_url(self, url: str):
-        pass
-
-    @abstractmethod
-    async def parse_rss(self, root) -> list:
+    async def parse_rss(self, root, category) -> list:
         pass
 
     async def fetch_rss(self, url: str):
@@ -58,6 +88,10 @@ class ScraperStrategy(ABC):
             # Return the root element
             return root
 
+    def _cname(self):
+        # return in Title Case
+        return self.__class__.__name__
+
     def parse_date(self, date):
         # Parse the date
         date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
@@ -70,69 +104,74 @@ class ScraperStrategy(ABC):
 
 
 class GMANewsScraper(ScraperStrategy):
-    rss_url = "https://data.gmanetwork.com/gno/rss/[category]/feed.xml"
-    category_mapping = {
-        Category.News: "news",
-        Category.Opinion: "opinion",
-        Category.Sports: "sports",
-        Category.Technology: "scitech",
-        Category.Lifestyle: "lifestyle",
-        Category.Business: "money",
-        Category.Entertainment: "showbiz",
-    }
+    @property
+    def _rss_url(self) -> str:
+        return "https://data.gmanetwork.com/gno/rss/[category]/feed.xml"
 
-    async def scrape_all(self):
-        for category in self.category_mapping:
-            self.scrape_category(category)
+    @property
+    def _category_mapping(self) -> dict:
+        return {
+            Category.News: "news",
+            Category.Opinion: "opinion",
+            Category.Sports: "sports",
+            Category.Technology: "scitech",
+            Category.Lifestyle: "lifestyle",
+            Category.Business: "money",
+            Category.Entertainment: "showbiz",
+        }
 
-    async def scrape_category(self, category: Category):
-        if category in self.category_mapping:
-            mapped_category = self.category_mapping[category]
-            print(f"Scraping GMA News for {category} ({mapped_category})")
-        else:
-            print(f"Category mapping not defined for GMA News: {category}")
+    async def parse_rss(self, root, category) -> list:
+        articles = []
+        # Iterate through each 'item' in the RSS feed
+        for item in root.findall(".//item"):
+            title = item.find("title").text
+            link = item.find("link").text
+            # description = item.find("description").text
+            pub_date = item.find("pubDate").text
 
-    async def scrape_url(self, url: str):
-        return super().scrape_url(url)
+            # Extracting media content URL (image URL)
+            media_content = item.find(
+                ".//media:content",
+                namespaces={"media": "http://search.yahoo.com/mrss/"},
+            )
+            image_url = media_content.get("url") if media_content is not None else None
 
+            # Create a dictionary for each article
+            article = {
+                "title": title,
+                "category": self.get_reversed_mapping()[category].value,
+                "source": Provider.GMANews.value,
+                "url": link,
+                # "description": description,
+                "date": self.parse_date(pub_date),
+                "image_url": image_url,
+            }
 
-class PhilstarScraper(ScraperStrategy):
-    category_mapping = {Category.Opinion: "Opinion"}
-
-    async def scrape_all(self):
-        for category in self.category_mapping:
-            self.scrape_category(category)
-
-    async def scrape_category(self, category: Category):
-        if category in self.category_mapping:
-            mapped_category = self.category_mapping[category]
-            print(f"Scraping Philstar for {category} ({mapped_category})")
-        else:
-            print(f"Category mapping not defined for Philstar: {category}")
-
-    async def scrape_url(self, url: str):
-        return super().scrape_url(url)
+            # Append the dictionary to the list of articles
+            articles.append(article)
 
 
 class NewsScraper:
     async def __init__(self, strategy: ScraperStrategy):
         self.strategy = strategy
 
-    async def scrape_all(self):
+    async def scrape_all(self) -> list:
         return self.strategy.scrape_all()
 
-    async def scrape_category(self, category: Category):
-        return self.strategy.scrape(category)
+    async def scrape_category(self, category: Category) -> list:
+        return self.strategy.scrape_category(category)
 
-    async def scrape_url(self, url: str):
+    async def scrape_url(self, url: str) -> dict:
         return self.strategy.scrape_url(url)
 
 
 # Define a mapping between Provider and ScraperStrategy
 provider_strategy_mapping = {
     Provider.GMANews: GMANewsScraper(),
-    Provider.Philstar: PhilstarScraper(),
-    # Add mappings for other providers
+    # Provider.Philstar: PhilstarScraper(),
+    # Provider.News5: News5Scraper(),
+    # Provider.ManilaBulletin: ManilaBulletinScraper(),
+    # Provider.INQUIRER: InquirerScraper(),
 }
 
 
@@ -142,10 +181,7 @@ async def get_scraper_strategy(provider: Provider) -> ScraperStrategy:
 
 # Example usage:
 gma_scraper = GMANewsScraper()
-philstar_scraper = PhilstarScraper()
 
 news_scraper_gma = NewsScraper(gma_scraper)
-news_scraper_philstar = NewsScraper(philstar_scraper)
 
 news_scraper_gma.scrape_category(Category.Technology)
-news_scraper_philstar.scrape_category(Category.Opinion)
