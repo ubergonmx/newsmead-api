40a14fe83af6011a93f993ded448a719c09e547c
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-01 01:49:00 +0800

    Add logging, insert and get_articles function, temp db conn

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,8 @@
 import sqlite3, os
+import logging
+
+# Logging
+logger = logging.getLogger(__name__)
 
 # Configuration
 db_name = "newsmead.sqlite"
@@ -8,6 +12,11 @@ db_insert_query = f"""
     (date, category, source, title, author, url, body, image_url, read_time)
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
     """
+
+
+# Get DB helper funciton
+def get_db():
+    return sqlite3.connect(db_name)
 
 
 def db_path():
@@ -21,7 +30,28 @@ def run_query(conn, query):
 
 
 def get_articles(conn):
-    return conn.execute(f"SELECT * FROM {db_tbl_articles}").fetchall()
+    conn = get_db() if conn is None else conn
+    articles = conn.execute(f"SELECT * FROM {db_tbl_articles}").fetchall()
+    cleaned_articles = []
+
+    # date, category, source, title, author, url, body, image_url, read_time
+
+    for article in articles:
+        cleaned_articles.append(
+            {
+                "date": article[1],
+                "category": article[2],
+                "source": article[3],
+                "title": article[4],
+                "author": article[5],
+                "url": article[6],
+                "body": article[7],
+                "imageUrl": article[8],
+                "readTime": article[9],
+            }
+        )
+
+    return cleaned_articles
 
 
 def table_exists(conn, table_name):
@@ -73,6 +103,34 @@ def insert_data(conn, data, insert_query=db_insert_query):
 
     conn.cursor().executemany(insert_query, data)
     conn.commit()
+
+
+def insert_articles(conn, articles):
+    conn = get_db() if conn is None else conn
+    data = []
+    for article in articles:
+        # Check if article is dict or not None
+        if not isinstance(article, dict) or article is None:
+            logger.warning(f"Article is not a dict or is None: {article}")
+            continue
+
+        logger.info(f"Inserting article: {article['title']}")
+        # Convert the article to a tuple and add it to the data list
+        # (date, category, source, title, author, url, body, image_url, read_time)
+        data.append(
+            (
+                article["date"],
+                article["category"],
+                article["source"],
+                article["title"],
+                article["author"],
+                article["url"],
+                article["body"],
+                article["image_url"],
+                article["read_time"],
+            )
+        )
+    insert_data(conn=conn, data=data)
 
 
 # Fix this
