3d848261b14501646d8d4c12dbd442ebcfd3da56
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-04 04:51:57 +0800

    Refactor ScraperStrategy and add save rss and config

--- newsscraper.py
+++ newsscraper.py
@@ -1,13 +1,16 @@
 from abc import ABC, abstractmethod
 from enum import Enum
+from typing import NamedTuple
 from bs4 import BeautifulSoup
 from newspaper import Article
 from datetime import datetime
+import os
 import httpx
 import asyncio
 import xml.etree.ElementTree as ET
 import readtime
 import logging
+import feedparser
 
 # [ ] TODO: Import typing module to be precise with types (especially return types)
 
@@ -33,39 +36,31 @@ class Provider(Enum):
     # INQUIRER = "inquirer"
 
 
+class Config(NamedTuple):
+    provider_name: str
+    category_mapping: dict
+    rss_url: str
+    default_author: str
+
+
 # [ ] TODO: Remove unnecessary comments
 # [ ] TODO: Add timer to benchmark performance
 class ScraperStrategy(ABC):
     @property
     @abstractmethod
-    def _category_mapping(self) -> dict:
-        pass
-
-    @property
-    @abstractmethod
-    def _rss_url(self) -> str:
+    def config(self) -> Config:
         pass
 
     async def scrape_all(self, proxy_scraper=None) -> list:
         results = []
-        for category in self._category_mapping:
+        for category in self.config.category_mapping:
             category_results = await self.scrape_category(category, proxy_scraper)
             results.extend(category_results)
         return results
 
     async def scrape_category(self, category: Category, proxy_scraper=None) -> list:
-        if category in self._category_mapping:
-            mapped_category = self._category_mapping[category]
-            log.info(f"{self._cname()} scraping for {category} ({mapped_category})")
-
-            # Replace the [category] placeholder with the mapped category
-            rss_url = self._rss_url.replace("[category]", mapped_category)
-
-            # Fetch and parse the RSS feed asynchronously
-            rss_root = await self.fetch_rss(rss_url)
-
-            # Extract URLs from the RSS feed
-            articles = await self.parse_rss(rss_root, mapped_category)
+        if category in self.config.category_mapping:
+            articles = await self.fetch_and_parse_rss(category)
 
             scraped_articles = await self.scrape_articles(articles, proxy_scraper)
 
@@ -101,117 +96,6 @@ class ScraperStrategy(ABC):
         log.info(f"{self._cname()} scraped {len(success)} articles")
         return success
 
-    @abstractmethod
-    async def scrape_article(self, article: dict, proxy: dict = None) -> tuple:
-        pass
-
-    async def scrape_article_with_retries(
-        self,
-        article,
-        proxy_scraper,
-        max_retries=10,
-    ) -> tuple:
-        for i in range(max_retries):
-            proxy = proxy_scraper.get_next_proxy()
-            log.info(
-                f"Using proxy: {list(proxy.values())[0]} ({i+1}/{max_retries}) -> {article['url']}"
-            )
-            result = await self.scrape_article(article, proxy)
-            if result[0]:
-                return result
-            else:
-                if i == max_retries - 1:  # If this was the last retry
-                    return (False, article)
-                else:
-                    continue  # Try again with the next proxy
-
-    @abstractmethod
-    async def parse_rss(self, root, category) -> list:
-        pass
-
-    async def fetch_rss(self, url: str):
-        async with httpx.AsyncClient() as client:
-            # Asynchronously download the RSS feed
-            rss_response = await client.get(url)
-
-            # Check if the RSS feed was successfully downloaded
-            if rss_response.status_code == 200:
-                # Parse the XML document
-                root = ET.fromstring(rss_response.content)
-            else:
-                # Print the error code
-                log.error(f"RSS status code: {rss_response.status_code}")
-                # Return an empty list
-                return []
-
-            # Return the root element
-            return root
-
-    def _cname(self):
-        # return in Title Case
-        return self.__class__.__name__
-
-    def parse_date(self, date):
-        # Parse the date
-        date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
-
-        # Return the date in the format Nov 01, 2020
-        return date.strftime("%b %d, %Y")
-
-    def _get_reversed_mapping(self, mapping):
-        return {v: k for k, v in mapping.items()}
-
-
-class GMANewsScraper(ScraperStrategy):
-    @property
-    def _rss_url(self) -> str:
-        return "https://data.gmanetwork.com/gno/rss/[category]/feed.xml"
-
-    @property
-    def _category_mapping(self) -> dict:
-        return {
-            Category.News: "news",
-            Category.Opinion: "opinion",
-            Category.Sports: "sports",
-            Category.Technology: "scitech",
-            Category.Lifestyle: "lifestyle",
-            Category.Business: "money",
-            Category.Entertainment: "showbiz",
-        }
-
-    async def parse_rss(self, root, category) -> list:
-        articles = []
-        reversed_mapping = self._get_reversed_mapping(self._category_mapping)
-        # Iterate through each 'item' in the RSS feed
-        for item in root.findall(".//item"):
-            title = item.find("title").text
-            link = item.find("link").text
-            # description = item.find("description").text
-            pub_date = item.find("pubDate").text
-
-            # Extracting media content URL (image URL)
-            media_content = item.find(
-                ".//media:content",
-                namespaces={"media": "http://search.yahoo.com/mrss/"},
-            )
-            image_url = media_content.get("url") if media_content is not None else None
-
-            # Create a dictionary for each article
-            article = {
-                "title": title,
-                "category": reversed_mapping[category].value,
-                "source": Provider.GMANews.value,
-                "url": link,
-                # "description": description,
-                "date": self.parse_date(pub_date),
-                "image_url": image_url,
-            }
-
-            # Append the dictionary to the list of articles
-            articles.append(article)
-
-        return articles
-
     async def scrape_article(self, article: dict, proxy: dict = None) -> tuple:
         async with httpx.AsyncClient(follow_redirects=True, proxies=proxy) as client:
             try:
@@ -241,7 +125,7 @@ class GMANewsScraper(ScraperStrategy):
 
             # Parse the article using newspaper3k
             news_article = Article(str(response.url))
-            news_article.download(input_html=str(response.content))
+            news_article.download(input_html=response.content)
             news_article.parse()
 
             # Add the article's body, author, and read time to the dictionary
@@ -251,7 +135,7 @@ class GMANewsScraper(ScraperStrategy):
                 if author is not None and news_article.authors[0] != author
                 else news_article.authors[0].strip()
                 if news_article.authors
-                else "GMA News Online"
+                else self.config.default_author
             )
 
             # Check if the author is all caps, convert to title case
@@ -261,6 +145,97 @@ class GMANewsScraper(ScraperStrategy):
             article["read_time"] = str(readtime.of_text(news_article.text))
 
         return (True, article)
+
+    async def scrape_article_with_retries(
+        self,
+        article,
+        proxy_scraper,
+        max_retries=10,
+    ) -> tuple:
+        for i in range(max_retries):
+            proxy = proxy_scraper.get_next_proxy()
+            log.info(
+                f"Using proxy: {list(proxy.values())[0]} ({i+1}/{max_retries}) -> {article['url']}"
+            )
+            result = await self.scrape_article(article, proxy)
+            if result[0]:
+                return result
+            else:
+                if i == max_retries - 1:
+                    return (False, article)
+                else:
+                    continue
+
+    async def fetch_and_parse_rss(self, category: Category, save=True) -> list:
+        articles = []
+        async with httpx.AsyncClient() as client:
+            mapped_category = self.config.category_mapping[category]
+            log.info(f"{self._cname()} scraping for {category} ({mapped_category})")
+
+            rss_url = self.config.rss_url.replace("[category]", mapped_category)
+            rss_response = await client.get(rss_url)
+
+            if rss_response.status_code == 200:
+                if save:
+                    await self.save_rss(rss_response.content, category)
+                feed = feedparser.parse(rss_response.content)
+
+                for entry in feed.entries:
+                    article = {
+                        "date": self.parse_date(entry.published),
+                        "category": category.value,
+                        "source": self.config.provider_name,
+                        "title": entry.title,
+                        "url": entry.link,
+                        "image_url": entry.media_content[0]["url"]
+                        if "media_content" in entry
+                        else None,
+                    }
+                    articles.append(article)
+            else:
+                log.error(f"RSS status code: {rss_response.status_code}")
+                return []
+
+            return articles
+
+    async def save_rss(self, rss_content: bytes, category: Category):
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        current_hr_min = datetime.now().strftime("%H-%M")
+
+        os.makedirs(f"feeds/{current_date}", exist_ok=True)
+
+        filename = f"feeds/{current_date}/{self.config.provider_name}-{category.value}-{current_hr_min}.xml"
+
+        with open(filename, "wb") as f:
+            f.write(rss_content)
+
+        log.info(f"Saved RSS feed to {filename}")
+
+    def _cname(self):
+        return self.__class__.__name__
+
+    def parse_date(self, date):
+        date = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %z")
+        return date.strftime("%b %d, %Y")
+
+
+class GMANewsScraper(ScraperStrategy):
+    @property
+    def config(self) -> Config:
+        return Config(
+            provider_name=Provider.GMANews.value,
+            category_mapping={
+                Category.News: "news",
+                Category.Opinion: "opinion",
+                Category.Sports: "sports",
+                Category.Technology: "scitech",
+                Category.Lifestyle: "lifestyle",
+                Category.Business: "money",
+                Category.Entertainment: "showbiz",
+            },
+            rss_url="https://data.gmanetwork.com/gno/rss/[category]/feed.xml",
+            default_author="GMA News Online",
+        )
 
 
 class NewsScraper:
