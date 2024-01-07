b4c937ab2399959a67e4157a33259bb02c6fde3b
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-07 23:19:56 +0800

    Add IP address to request logging

--- main.py
+++ main.py
@@ -112,7 +112,8 @@ jobs = [
 @app.middleware("http")
 async def log_requests(request: Request, call_next):
     idem = "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
-    log.info(f"rid={idem} start request path={request.url.path}")
+    ip = request.client.host
+    log.info(f"rid={idem} start request path={request.url.path} ip={ip}")
     start_time = time.time()
 
     response = await call_next(request)
@@ -120,7 +121,7 @@ async def log_requests(request: Request, call_next):
     process_time = (time.time() - start_time) * 1000
     formatted_process_time = "{0:.2f}".format(process_time)
     log.info(
-        f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code}"
+        f"rid={idem} completed_in={formatted_process_time}ms status_code={response.status_code} ip={ip}"
     )
 
     return response
