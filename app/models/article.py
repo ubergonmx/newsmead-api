0ff9884a97e109347f8eeb8b8df2b783382f0fff
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-25 04:28:45 +0800

    Add Article model

---
+++ app/models/article.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+
+
+class Article(BaseModel):
+    date: str
+    category: str
+    source: str
+    title: str
+    author: str = ""
+    url: str
+    body: str = ""
+    image_url: str = ""
+    read_time: str
