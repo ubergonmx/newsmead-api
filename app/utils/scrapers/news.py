f63506e6b85ecf4a99a193518f99db2f9d006c52
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-25 04:29:33 +0800

    Refactor newsscraper and add news provider strategies

--- app/utils/scrapers/news.py
+++ app/utils/scrapers/news.py
@@ -5,28 +5,22 @@ from abc import ABC, abstractmethod
 from enum import Enum
 from typing import NamedTuple
 from bs4 import BeautifulSoup
-from newspaper import Article
+from newspaper import Article as ArticleScraper
 from datetime import datetime
 from dateutil.parser import parse
 from fake_useragent import UserAgent
-from database_utils import url_exists
+from app.database.asyncdb import get_db
+from app.models.article import Article
+import app.backend.config as config
 import os
 import httpx
 import asyncio
-import xml.etree.ElementTree as ET
 import readtime
 import logging
 import feedparser
 
-# [ ] TODO: Import typing module to be precise with types (especially return types)
-
 # Configure logging
 log = logging.getLogger(__name__)
-
-# Configure feeds directory
-feeds_dir = "feeds"
-rss_dir = "rss"
-webcrawl_dir = "webcrawl"
 
 
 class Category(Enum):
@@ -49,34 +43,31 @@ class Provider(Enum):
 
 class ScraperConfig(NamedTuple):
     provider_name: str
-    category_mapping: dict
+    category_mapping: dict[Category, str]
     rss_url: str
     default_author: str
+    webcrawler_urls: dict[Category, list[str]] = None
 
 
-# [ ] TODO: Remove unnecessary comments
-# [ ] TODO: Add timer to benchmark performance
 class ScraperStrategy(ABC):
     @property
     @abstractmethod
     def config(self) -> ScraperConfig:
         pass
 
-    async def scrape_all(self, proxy_scraper=None) -> list:
+    async def scrape_all(self, proxy_scraper=None) -> list[Article]:
         results = []
-        for category in self.config.category_mapping:
+        for category in self.config.category_mapping.items():
             category_results = await self.scrape_category(category, proxy_scraper)
             results.extend(category_results)
         return results
 
-    async def scrape_category(self, category: Category, proxy_scraper=None) -> list:
+    async def scrape_category(
+        self, category: Category, proxy_scraper=None
+    ) -> list[Article]:
         if category in self.config.category_mapping:
             articles = await self.fetch_and_parse_rss(category)
-            filtered_articles = [
-                article
-                for article in articles
-                if not url_exists(article["url"], article["source"])
-            ]
+            filtered_articles = await get_db().filter_new_urls(articles)
             scraped_articles = await self.scrape_articles(
                 filtered_articles, proxy_scraper
             )
@@ -86,7 +77,9 @@ class ScraperStrategy(ABC):
         else:
             log.error(f"Category mapping not defined for {self._cname()}: {category}")
 
-    async def scrape_articles(self, articles: list, proxy_scraper=None) -> list:
+    async def scrape_articles(
+        self, articles: list[Article], proxy_scraper=None
+    ) -> list[Article]:
         tasks = [self.scrape_article(article) for article in articles]
         results = await asyncio.gather(*tasks)
 
@@ -113,73 +106,53 @@ class ScraperStrategy(ABC):
         log.info(f"{self._cname()} scraped {len(success)} articles")
         return success
 
-    async def scrape_article(self, article: dict, proxy: dict = None) -> tuple:
+    async def scrape_article(self, article: Article, proxy: dict = None) -> tuple:
         headers = {"User-Agent": UserAgent().random}
         async with httpx.AsyncClient(
             headers=headers, follow_redirects=True, proxies=proxy
         ) as client:
             try:
-                response = await client.get(article["url"])
+                response = await client.get(article.url)
                 response.raise_for_status()
             except httpx.HTTPError as e:
                 log.error(
                     f"HTTP Exception {'' if proxy is None else '(proxy: ' + list(proxy.values())[0] + ')'}: {e}"
                 )
-                return (False, article)
+                return False, article
 
-            # Check if the article is a video
-            if "video" in response.headers["content-type"]:
-                log.error(f"Article is a video: {article['url']}")
-                return (False, article)
+            if "video" in response.headers["content-type"] or not response.content:
+                log.error(f"Article is a video or has no content: {article.url}")
+                return False, article
 
-            # Check if the response has no content
-            if len(response.content) == 0:
-                log.error(f"Article has no content: {article['url']}")
-                return (False, article)
-
-            # Parse the HTML document with BeautifulSoup to get the author
             soup = BeautifulSoup(response.content, "html.parser")
-            author = soup.find("meta", {"name": "author"})
-            if author is not None:
-                author = author.get("content")
-                if "," in author:
-                    author = author.split(",")[0]
-                author = author.strip()
-            else:
-                author = None
+            author = self.extract_author(soup)
 
-            # Parse the article using newspaper3k
-            news_article = Article(str(response.url))
+            news_article = ArticleScraper(str(article.url))
             news_article.download(input_html=response.content)
             news_article.parse()
 
-            # Add the article's body, author, and read time to the dictionary
-            article["body"] = news_article.text
-            article["author"] = (
+            article.date = article.date or self.parse_date_complete(
+                news_article.publish_date
+            )
+            article.title = article.title or news_article.title
+            article.body = news_article.text
+            article.author = (
                 author
                 if author is not None and news_article.authors[0] != author
                 else news_article.authors[0].strip()
                 if news_article.authors
                 else self.config.default_author
             )
+            article.author = (
+                article.author.title() if article.author.isupper() else article.author
+            )
+            article.image_url = article.image_url or news_article.top_image
+            article.read_time = str(readtime.of_text(news_article.text))
 
-            # Check if the author is all caps, convert to title case
-            if article["author"].isupper():
-                article["author"] = article["author"].title()
-
-            # CHeck if image url is empty
-            if article["image_url"] is None:
-                article["image_url"] = news_article.top_image
-
-            article["read_time"] = str(readtime.of_text(news_article.text))
-
-        return (True, article)
+        return True, article
 
     async def scrape_article_with_retries(
-        self,
-        article,
-        proxy_scraper,
-        max_retries=10,
+        self, article: Article, proxy_scraper, max_retries=10
     ) -> tuple:
         for i in range(max_retries):
             proxy = proxy_scraper.get_next_proxy()
@@ -189,13 +162,10 @@ class ScraperStrategy(ABC):
             result = await self.scrape_article(article, proxy)
             if result[0]:
                 return result
-            else:
-                if i == max_retries - 1:
-                    return (False, article)
-                else:
-                    continue
+            elif i == max_retries - 1:
+                return (False, article)
 
-    async def fetch_and_parse_rss(self, category: Category, save=True) -> list:
+    async def fetch_and_parse_rss(self, category: Category, save=True) -> list[Article]:
         articles = []
         async with httpx.AsyncClient() as client:
             mapped_category = self.config.category_mapping[category]
@@ -208,52 +178,75 @@ class ScraperStrategy(ABC):
                 f"{self._cname()} scraping for {category} ({mapped_category}) - {rss_url}"
             )
 
-            if rss_response.status_code == 200:
-                if save:
-                    await self.save_rss(rss_response.content, category)
-                feed = feedparser.parse(rss_response.content)
-
-                for entry in feed.entries:
-                    article = {
-                        "date": self.parse_date(entry.published),
-                        "category": category.value,
-                        "source": self.config.provider_name,
-                        "title": entry.title,
-                        "url": entry.link,
-                        "image_url": entry.media_content[0]["url"]
-                        if "media_content" in entry
-                        else None,
-                    }
-                    articles.append(article)
-            else:
+            if rss_response.status_code != 200:
                 log.error(f"RSS status code: {rss_response.status_code}")
                 return []
+
+            if save:
+                await self.save_rss(rss_response.content, category)
+            feed = feedparser.parse(rss_response.content)
+
+            for entry in feed.entries:
+                article = Article(
+                    date=self.parse_date_complete(entry.published),
+                    category=category.value,
+                    source=self.config.provider_name,
+                    title=entry.title,
+                    url=entry.link,
+                    image_url=entry.media_content[0]["url"]
+                    if "media_content" in entry
+                    else None,
+                )
+                articles.append(article)
 
             return articles
 
     async def save_rss(self, rss_content: bytes, category: Category):
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        current_hr_min = datetime.now().strftime("%H-%M")
+        current_time = datetime.now().strftime("%Y-%m-%d-%H-%M")
 
-        os.makedirs(f"{feeds_dir}/{rss_dir}/{current_date}", exist_ok=True)
-
-        filename = f"{feeds_dir}/{rss_dir}/{current_date}/{self.config.provider_name}-{category.value}-{current_hr_min}.xml"
+        filename = os.path.join(
+            config.get_rss_dir(),
+            current_time[:10],  # Get the date part
+            f"{self.config.provider_name}-{category.value}-{current_time[11:]}.xml",  # Get the time part
+        )
 
         with open(filename, "wb") as f:
             f.write(rss_content)
 
         log.info(f"Saved RSS feed to {filename}")
 
-    def _cname(self):
+    async def save_webcrawler(self, webcrawler_content: bytes, category: Category):
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        current_hr_min = datetime.now().strftime("%H-%M")
+
+        filename = os.path.join(
+            config.get_webcrawler_dir(),
+            current_date,
+            f"{self.config.provider_name}-{category.value}-{current_hr_min}.xml",
+        )
+
+        with open(filename, "wb") as f:
+            f.write(webcrawler_content)
+
+        log.info(f"Saved webcrawler feed to {filename}")
+
+    def extract_author(soup: BeautifulSoup) -> str:
+        author = soup.find("meta", {"name": "author"})
+        if author is not None:
+            return author.get("content").split(",")[0].strip()
+        return None
+
+    def _cname(self) -> str:
         return self.__class__.__name__
 
-    def parse_date(self, date):
-        date = parse(date)
-        return date.strftime("%b %d, %Y")
+    def parse_date_complete(self, date) -> str:
+        return parse(date).strftime("%b %d, %Y-%I:%M %p")
 
-    def parse_time(self, time):
-        time = parse(time)
-        return time.strftime("%I:%M %p")
+    def parse_date(self, date) -> str:
+        return parse(date).strftime("%b %d, %Y")
+
+    def parse_time(self, time) -> str:
+        return parse(time).strftime("%I:%M %p")
 
 
 class GMANewsScraper(ScraperStrategy):
@@ -275,23 +268,31 @@ class GMANewsScraper(ScraperStrategy):
         )
 
 
-# class PhilstarScraper(ScraperStrategy):
-#     @property
-#     def config(self) -> Config:
-#         return Config(
-#             provider_name=Provider.Philstar.value,
-#             category_mapping={
-#                 Category.News: "headlines",
-#                 Category.Opinion: "opinion",
-#                 Category.Sports: "sports",
-#                 Category.Technology: "technology",
-#                 Category.Lifestyle: "lifestyle",
-#                 Category.Business: "business",
-#                 Category.Entertainment: "entertainment",
-#             },
-#             rss_url="https://www.philstar.com/rss/[category]",
-#             default_author="Philstar.com",
-#         )
+class PhilstarScraper(ScraperStrategy):
+    @property
+    def config(self) -> ScraperConfig:
+        return ScraperConfig(
+            provider_name=Provider.Philstar.value,
+            category_mapping={
+                Category.News: "headlines",
+                Category.Opinion: "opinion",
+                Category.Sports: "sports",
+                Category.Technology: "business/technology",
+                Category.Lifestyle: "lifestyle",
+                Category.Business: "business",
+                Category.Entertainment: "entertainment",
+            },
+            rss_url="https://www.philstar.com/rss/[category]",
+            default_author="Philstar.com",
+        )
+
+    async def extract_author(soup: BeautifulSoup) -> str:
+        author_tag = soup.find("div", class_="article__credits-author-pub")
+        return (
+            getattr(author_tag.find_all("a")[-1], "text", author_tag.text).strip()
+            if author_tag
+            else None
+        )
 
 
 class ManilaBulletinScraper(ScraperStrategy):
@@ -304,6 +305,31 @@ class ManilaBulletinScraper(ScraperStrategy):
             },
             rss_url="https://mb.com.ph/rss/[category]",
             default_author="Manila Bulletin",
+        )
+
+    async def extract_author(soup: BeautifulSoup) -> str:
+        author_tag = soup.find(
+            "a", class_="custom-text-link uppercase author-name-link pb-0 mt-1"
+        )
+        return author_tag.span.text if author_tag else None
+
+
+class InquirerScraper(ScraperStrategy):
+    @property
+    def config(self) -> ScraperConfig:
+        return ScraperConfig(
+            provider_name=Provider.INQUIRER.value,
+            category_mapping={
+                Category.News: "newsinfo",
+                Category.Opinion: "opinion",
+                Category.Sports: "sports",
+                Category.Technology: "technology",
+                Category.Lifestyle: "lifestyle",
+                Category.Business: "business",
+                Category.Entertainment: "entertainment",
+            },
+            rss_url="https://[category].inquirer.net/feed",
+            default_author="INQUIRER.net",
         )
 
 
@@ -327,10 +353,10 @@ class NewsScraper:
 # Define a mapping between Provider and ScraperStrategy
 provider_strategy_mapping = {
     Provider.GMANews: GMANewsScraper(),
-    # Provider.Philstar: PhilstarScraper(),
+    Provider.Philstar: PhilstarScraper(),
     # Provider.News5: News5Scraper(),
     Provider.ManilaBulletin: ManilaBulletinScraper(),
-    # Provider.INQUIRER: InquirerScraper(),
+    Provider.INQUIRER: InquirerScraper(),
 }
 
 
