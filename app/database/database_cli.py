63f10905c78429cf986e3939713a72fb1797c15d
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-29 04:57:54 +0800

    Refactor database_cli.py and database_utils.py

--- app/database/database_cli.py
+++ app/database/database_cli.py
@@ -1,47 +1,31 @@
+from datetime import datetime
 import sqlite3
-import os
 import json
-import logging
 from database_utils import (
     run_query,
     table_exists,
     show_table,
-    drop_table,
     create_article_table,
-    db_name,
+    db_path,
 )
 
-# Configure logging
-log = logging.getLogger(__name__)
-
-
-# db_name = input("Enter database name: ") + ".sqlite"
-# if not db_exists(db_name):
-#     if input(f"Database does not exist. Create? (y/n): ") == "n":
-#         exit()
-
-# Get the directory of the current script
-script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Join the script directory with the database name
-db_path = os.path.join(script_dir, db_name)
-
 # Connect to the SQLite database
-conn = sqlite3.connect(db_path)
+conn = sqlite3.connect(db_path())
 
-table_name = input("Enter table name: ")
+# table_name = input("Enter table name: ")
 
-# [ ] TODO: Refactor table creation - involve database_utils.py
-if not (table_exists(conn, table_name)):
-    if input(f'Table "{table_name}" does not exist. Create? (y/n): ') == "y":
-        create_article_table(conn, table_name)
-    else:
-        conn.close()
-        quit()
+# if not (table_exists(conn, table_name)):
+#     if input(f'Table "{table_name}" does not exist. Create? (y/n): ') == "y":
+#         create_article_table(conn, table_name)
+#     else:
+#         conn.close()
+#         quit()
+
+table_name = "articles"
 
 while True:
     ans = input(
-        "\n\nWhat do you want to do?\n 1) Show table \n 2) Reset table (DISABLED) \n 3) Enter SQL query \n q) Exit \n\n >"
+        "\n\nWhat do you want to do?\n 1) Show table \n 2) Custom script \n 3) Enter SQL query \n q) Exit \n\n >"
     )
 
     if ans == "1":
@@ -51,12 +35,40 @@ while True:
             continue
         print(json.dumps(table, indent=2))
     elif ans == "2":
-        # [ ] TODO: Add backup functionality before dropping table. Discuss with team.
-        # if input("This process is NOT reversible. Are you sure (y/n): ") == "y":
-        #     drop_table(conn, table_name)
-        #     create_article_table(conn, table_name)
+        # This block is for custom scripts
+        c = conn.cursor()
 
-        # print(f'Reset "{table_name}".')
+        # Select all rows from the articles table
+        c.execute("SELECT article_id, date FROM articles")
+        rows = c.fetchall()
+
+        # For each row
+        for row in rows:
+            id, date = row
+
+            # Try to parse the date and convert it to the desired format
+            try:
+                new_date = datetime.strptime(date, "%b %d, %Y-%I:%M %p").strftime(
+                    "%Y-%m-%d %H:%M:%S"
+                )
+            except ValueError:
+                try:
+                    new_date = datetime.strptime(date, "%b %d, %Y").strftime(
+                        "%Y-%m-%d %H:%M:%S"
+                    )
+                except ValueError:
+                    print(f"Could not parse date: {date}")
+                    continue
+
+            # Update the date in the database
+            c.execute(
+                "UPDATE articles SET date = ? WHERE article_id = ?", (new_date, id)
+            )
+
+        # Commit the changes and close the connection
+        conn.commit()
+
+        # End of custom scripts
         conn.close()
         exit()
 
--- app/database/database_utils.py
+++ app/database/database_utils.py
@@ -1,12 +1,15 @@
 import sqlite3
 import os
 import logging
+import dotenv
+
+dotenv.load_dotenv()
 
 # Configure logging
 log = logging.getLogger(__name__)
 
 # Configuration
-db_name = os.getenv("DB_NAME", "news.db")
+db_name = os.getenv("DB_NAME", "newsmead.sqlite")
 db_tbl_articles = "articles"
 
 # [ ] TODO: Refactor queries
@@ -48,8 +51,12 @@ def get_db():
 
 
 def db_path():
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(script_dir, db_name)
+    project_dir_name = os.getenv("PROJECT_DIR_NAME")
+    current_file_path = os.path.abspath(__file__)
+    path_components = current_file_path.split(os.sep)
+    project_root_index = path_components.index(project_dir_name)
+    project_root = os.sep.join(path_components[: project_root_index + 1])
+    return os.path.join(project_root, db_name)
 
 
 def run_query(conn, query):
@@ -123,8 +130,6 @@ def table_exists(conn, table_name):
 
 
 def create_article_table(conn, table_name):
-    # Create the table with the following columns
-    # article_id, date, category, source, title, author, url, body, image_url, read_time
     run_query(conn, db_create_article_table_query)
 
 
@@ -135,97 +140,3 @@ def drop_table(conn, table_name):
 
 def show_table(conn, table_name):
     return conn.execute(f"SELECT * FROM {table_name}").fetchall()
-
-
-# [ ] TODO: Refactor insert functions and add optional table_name parameter
-def insert_data(conn, data, insert_query=db_insert_query):
-    # Create the table if it does not exist
-    if not table_exists(conn, db_tbl_articles):
-        create_article_table(conn, db_tbl_articles)
-
-    conn.cursor().executemany(insert_query, data)
-    conn.commit()
-
-
-# [ ] TODO: Discuss with team if bulk insert or insert one by one
-def insert_articles(conn, articles):
-    conn = get_db() if conn is None else conn
-
-    # Create the table if it does not exist
-    if not table_exists(conn, db_tbl_articles):
-        create_article_table(conn, db_tbl_articles)
-
-    new_articles = []
-    existing_urls = set(
-        url[0] for url in conn.execute(f"SELECT url FROM {db_tbl_articles}").fetchall()
-    )
-    invalid_count = 0
-    existing_count = 0
-    empty_count = 0
-    for article in articles:
-        # Check if article is dict or not None
-        if not isinstance(article, dict) or article is None:
-            log.warning(f"Article is not a dict or is None")
-            invalid_count += 1
-            continue
-
-        # Check if article already exists in the database
-        if article["url"] in existing_urls:
-            log.info(f"Article already exists: {article['title']}")
-            existing_count += 1
-            continue
-
-        # Check if "body" key exists and article body is empty
-        if "body" not in article or article["body"] == "":
-            log.info(f"Article body is empty: {article['title']}")
-            empty_count += 1
-            continue
-
-        # Log the article to be inserted
-        log_article = article.copy()
-        log_article["body"] = log_article["body"][:10]
-        log.info(f"Inserting article: {log_article}")
-
-        # Convert the article to a tuple and add it to the data list
-        # (date, category, source, title, author, url, body, image_url, read_time)
-        new_articles.append(
-            (
-                article["date"],
-                article["category"],
-                article["source"],
-                article["title"],
-                article.get("author", ""),
-                article["url"],
-                article.get("body", ""),
-                article["image_url"],
-                article.get("read_time", ""),
-            )
-        )
-    insert_data(conn=conn, data=new_articles)
-    log.info(
-        f"Inserted {len(new_articles)}/{len(articles)} (dup:-{existing_count}, inv:-{invalid_count}, emt:+{empty_count}, ok=+{len(new_articles)-empty_count}) articles."
-    )
-
-
-def delete_duplicates(conn):
-    conn = get_db() if conn is None else conn
-    run_query(conn, db_delete_duplicates_query)
-    log.info(f"Deleted duplicates.")
-
-
-def url_exists(conn, url):
-    cursor = get_db().cursor() if conn is None else conn.cursor()
-    cursor.execute("SELECT 1 FROM articles WHERE url=?", (url,))
-    return cursor.fetchone() is not None
-
-
-def delete_empty_body_by_provider(conn, provider):
-    conn = get_db() if conn is None else conn
-    run_query(
-        conn,
-        f"""
-        DELETE FROM {db_tbl_articles}
-        WHERE body = '' and source = '{provider}'
-    """,
-    )
-    log.info(f"Deleted empty body articles from {provider}.")
