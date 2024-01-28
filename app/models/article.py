e7a5fe71fb5ede3d9d3c89c627ea600e467d590a
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-29 03:28:14 +0800

    Add Filter class to Article model

--- app/models/article.py
+++ app/models/article.py
@@ -13,3 +13,11 @@ class Article(BaseModel):
     body: str = ""
     image_url: str = ""
     read_time: str = ""
+
+
+class Filter(BaseModel):
+    category: Optional[str] = None
+    source: Optional[str] = None
+    sortBy: Optional[str] = None
+    startDate: Optional[str] = None
+    endDate: Optional[str] = None
