5087c770bef2129adf5c1858a52781e43221d155
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-29 07:17:27 +0800

    Refactor API endpoints and add query parameters for filtering and pagination

--- app/api/articles.py
+++ app/api/articles.py
@@ -1,31 +1,57 @@
 from sqlite3 import IntegrityError
-from fastapi import APIRouter, HTTPException, Depends
-from app.database.asyncdb import AsyncDatabase, get_db
-from app.models.article import Article
+from typing import Optional
+from fastapi import APIRouter, HTTPException, Depends, Query
+from app.database.asyncdb import AsyncDatabase
+from app.models.article import Article, Filter
 
 router = APIRouter()
 
 
+async def get_db():
+    async with AsyncDatabase() as db:
+        yield db
+
+
 @router.get("/")
-async def get_articles():
-    pass
+async def get_articles(
+    source: Optional[str] = Query(None),
+    category: Optional[str] = Query(None),
+    startDate: Optional[str] = Query(None),
+    endDate: Optional[str] = Query(None),
+    text: Optional[str] = Query(None),
+    sortBy: Optional[str] = Query(None),
+    page: int = Query(1),
+    page_size: int = Query(10),
+    db: AsyncDatabase = Depends(AsyncDatabase),
+):
+    filter = Filter(
+        source=source,
+        category=category,
+        startDate=startDate,
+        endDate=endDate,
+        text=text,
+        sortBy=sortBy,
+    )
+    # try:
+    articles = await db.get_articles(filter, page, page_size)
+    return articles
+    # except Exception as e:
+    #     raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.get("/{id}")
-async def get_article(id: int):
-    pass
-
-
-@router.get("/search")
-async def search_articles(q: str):
-    pass
+@router.get("/{article_id}")
+async def get_article(article_id: int, db: AsyncDatabase = Depends(get_db)):
+    # try:
+    article = await db.get_article_by_id(article_id)
+    return article
+    # except Exception as e:
+    #     raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.post("/new")
-async def create_article(article: Article, db: AsyncDatabase = Depends(get_db)):
+async def create_article(article: Article, db: AsyncDatabase = Depends(AsyncDatabase)):
     try:
-        await db.create_article_table()  # Ensure table exists
-        await db.insert_data([article])  # Insert data
+        await db.insert_data([article])
         return {"message": "Article created successfully"}
     except IntegrityError as e:
         raise HTTPException(
