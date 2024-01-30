f9fbd7770c9a6b7d10baeda818ff44de2b5ffcbb
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-31 05:06:09 +0800

    Add language detection functionality to the NLP module

---
+++ app/utils/nlp/lang.py
@@ -0,0 +1,44 @@
+from lingua import Language, LanguageDetectorBuilder
+
+
+class Lang:
+    def __init__(self, all=False):
+        self.detector = (
+            LanguageDetectorBuilder.from_all_languages()
+            if all
+            else LanguageDetectorBuilder.from_languages(
+                Language.ENGLISH, Language.TAGALOG
+            ).build()
+        )
+
+    def detect(self, text) -> str:
+        """
+        Returns the top language of the text.
+
+        Example:
+        ```
+        "TAGALOG"
+        ```
+        """
+        return self.detector.detect_language_of(text).name
+
+    def detect_with_score(self, text) -> dict[str, float]:
+        """
+        Returns a dict with the top language and the confidence score.
+
+        Example:
+        ```
+        {
+            "lang": "TAGALOG",
+            "score": 0.9585779901734812
+        }
+        ```
+        """
+        result = self.detector.compute_language_confidence_values(text)
+        return {"lang": result[0].language.name, "score": result[0].value}
+
+    def is_en(self, text) -> bool:
+        """
+        Returns True if the text is in English.
+        """
+        return self.detect(text) == "ENGLISH"
