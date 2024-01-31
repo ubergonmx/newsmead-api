234c54a3986e449a769547e3ef858b0594ad42fb
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-31 22:39:11 +0800

    Fix write_chunk_to_tsv and add limit words for body

--- app/core/recommender.py
+++ app/core/recommender.py
@@ -52,10 +52,35 @@ class Recommender:
     # def load_news(self, news_file: str = None):
     #     self.model.news_vecs = self.model.run_news(news_file or self.news_file)
 
+    def limit_words(self, text: str, limit: int = None) -> str:
+        limit = limit or self.model.hparams.body_size
+        clean_text = (
+            text.replace("\n", " ")
+            .replace("/", "")
+            .replace("\\", "")
+            .replace(r"\u2014", "")
+            .strip()
+        )
+        words = clean_text.split()
+        return " ".join(words[:limit])
+
     async def write_chunk_to_tsv(self, chunk, filename):
-        async with aiofiles.open(filename, "a") as f:
-            writer = AsyncWriter(f, delimiter="\t")
-            await writer.writerows(chunk)
+        async with aiofiles.open(filename, "a", encoding="utf-8") as f:
+            writer = AsyncWriter(f, delimiter="\t", newline="")
+            for row in chunk:
+                # article_id:0, category:2, title:4, body:7, url:6
+                await writer.writerow(
+                    [
+                        row[0],
+                        row[2],
+                        row[2],
+                        row[4],
+                        self.limit_words(row[7]),
+                        row[6],
+                        "[skip]",
+                        "[skip]",
+                    ]
+                )
 
     async def save_news(
         self, db: AsyncDatabase, chunk_size: int = 1000, news_file: str = None
@@ -70,6 +95,8 @@ class Recommender:
                 break
 
             await self.write_chunk_to_tsv(chunk, news_file)
+
+        log.info(f"Saved news to {news_file}")
 
     async def load_news(self, news_file: str = None):
         loop = asyncio.get_event_loop()
