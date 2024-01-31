b84eb4958833c4b2a3e63dc06e8137ccc44a6d3c
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-31 23:24:14 +0800

    Refactor asynchronous loading of news to synchronous loading

--- app/backend/event_scheduler.py
+++ app/backend/event_scheduler.py
@@ -27,7 +27,7 @@ async def check_and_fix_empty_articles(recommender: "Recommender"):
             articles = await news_scraper.scrape_articles(empty_articles, proxy)
             await db.update_empty_articles(articles)
         await recommender.save_news(db)
-    await recommender.load_news()
+    recommender.load_news_sync()
 
 
 async def scrape_all_providers(recommender: "Recommender"):
@@ -41,7 +41,7 @@ async def scrape_all_providers(recommender: "Recommender"):
             articles = await news_scraper.scrape_all(proxy)
             await db.insert_articles(articles)
         await recommender.save_news(db)
-    await recommender.load_news()
+    recommender.load_news_sync()
 
 
 # Scheduler jobs
