b1d20f0114e72e0d380d5e34bdb17512b6aa7008
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-02-01 01:35:52 +0800

    Refactor file paths and temporary directory in train_large.py

--- app/core/train_large.py
+++ app/core/train_large.py
@@ -29,17 +29,18 @@ batch_size = 32
 # Options: demo, small, large
 MIND_type = "large"
 tmp_dir = TemporaryDirectory()
-print(f"Temporary directory: {tmp_dir.name}")
-train_news_file = os.path.join(tmp_dir, "train", r"news.tsv")
-train_behaviors_file = os.path.join(tmp_dir, "train", r"behaviors.tsv")
-valid_news_file = os.path.join(tmp_dir, "valid", r"news.tsv")
-valid_behaviors_file = os.path.join(tmp_dir, "valid", r"behaviors.tsv")
-wordEmb_file = os.path.join(tmp_dir, "utils", "embedding_all.npy")
-userDict_file = os.path.join(tmp_dir, "utils", "uid2index.pkl")
-wordDict_file = os.path.join(tmp_dir, "utils", "word_dict_all.pkl")
-vertDict_file = os.path.join(tmp_dir, "utils", "vert_dict.pkl")
-subvertDict_file = os.path.join(tmp_dir, "utils", "subvert_dict.pkl")
-yaml_file = os.path.join(tmp_dir, "utils", r"naml.yaml")
+data_path = tmp_dir.name
+print(f"Temporary directory: {data_path}")
+train_news_file = os.path.join(data_path, "train", r"news.tsv")
+train_behaviors_file = os.path.join(data_path, "train", r"behaviors.tsv")
+valid_news_file = os.path.join(data_path, "valid", r"news.tsv")
+valid_behaviors_file = os.path.join(data_path, "valid", r"behaviors.tsv")
+wordEmb_file = os.path.join(data_path, "utils", "embedding_all.npy")
+userDict_file = os.path.join(data_path, "utils", "uid2index.pkl")
+wordDict_file = os.path.join(data_path, "utils", "word_dict_all.pkl")
+vertDict_file = os.path.join(data_path, "utils", "vert_dict.pkl")
+subvertDict_file = os.path.join(data_path, "utils", "subvert_dict.pkl")
+yaml_file = os.path.join(data_path, "utils", r"naml.yaml")
 
 mind_url, mind_train_dataset, mind_dev_dataset, mind_utils = get_mind_data_set(
     MIND_type
@@ -47,17 +48,17 @@ mind_url, mind_train_dataset, mind_dev_dataset, mind_utils = get_mind_data_set(
 
 if not os.path.exists(train_news_file):
     download_deeprec_resources(
-        mind_url, os.path.join(tmp_dir, "train"), mind_train_dataset
+        mind_url, os.path.join(data_path, "train"), mind_train_dataset
     )
 
 if not os.path.exists(valid_news_file):
     download_deeprec_resources(
-        mind_url, os.path.join(tmp_dir, "valid"), mind_dev_dataset
+        mind_url, os.path.join(data_path, "valid"), mind_dev_dataset
     )
 if not os.path.exists(yaml_file):
     download_deeprec_resources(
         r"https://recodatasets.z20.web.core.windows.net/newsrec/",
-        os.path.join(tmp_dir, "utils"),
+        os.path.join(data_path, "utils"),
         mind_utils,
     )
 
@@ -92,7 +93,7 @@ print(f"Model evaluation time: {timedelta(seconds=time.time() - start_time)}")
 print(f"Saving results...")
 start_time = time.time()
 
-res_path = os.path.join(tmp_dir, "utils", "evaluation_results.txt")
+res_path = os.path.join(data_path, "utils", "evaluation_results.txt")
 with open(res_path, "w") as f:
     f.write(f"group_auc: {res['group_auc']}\n")
     f.write(f"mean_mrr: {res['mean_mrr']}\n")
@@ -104,7 +105,7 @@ print("Saving model...")
 start_time = time.time()
 
 # Save the model
-model_path = os.path.join(tmp_dir, "pretrained")
+model_path = os.path.join(data_path, "pretrained")
 os.makedirs(model_path, exist_ok=True)
 model.model.save_weights(os.path.join(model_path, "naml_ckpt"))
 
@@ -117,7 +118,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 rec_path = os.path.join(script_dir, "recommender_utils")
 os.makedirs(rec_path, exist_ok=True)
 for folder in ["pretrained", "utils"]:
-    shutil.move(os.path.join(tmp_dir, folder), os.path.join(rec_path, folder))
+    shutil.move(os.path.join(data_path, folder), os.path.join(rec_path, folder))
 print(f"Model moving time: {time.time() - start_time}")
 
 print("Cleaning up...")
