0cffc0887e83689ce8658bacc680562b3045544d
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-02-04 18:56:21 +0800

    Add recommended_articles endpoint to recommender.py

--- app/api/recommender.py
+++ app/api/recommender.py
@@ -1,11 +1,43 @@
 from sqlite3 import IntegrityError
-from fastapi import APIRouter, File, UploadFile, HTTPException, Depends, Header
+from fastapi import APIRouter, File, Request, UploadFile, HTTPException, Depends, Header
 from app.database.asyncdb import AsyncDatabase, get_db
-from app.models.article import Article
+from app.models.article import Article, Filter
+from datetime import datetime
+from pytz import timezone
 import app.backend.config as config
 import os
+import logging
 
+log = logging.getLogger(__name__)
 router = APIRouter()
+
+
+@router.get("/{user_id}/{page}")
+async def recommended_articles(
+    user_id: str, page: int, request: Request, db: AsyncDatabase = Depends(get_db)
+):
+    history = await db.get_user_history(user_id)
+    articles = await db.get_articles(Filter(), page, 10)
+    log.info(f"history: {history}")
+    log.info(f"articles count: {len(articles)}")
+    if history:
+        impression = [
+            f"{article.article_id}-{'1' if article.article_id in history else '0'}"
+            for article in articles
+        ]
+        log.info(f"impression: {impression}")
+        time_now = datetime.now(timezone("Asia/Manila"))
+        behavior = f"{user_id}\t{time_now}\t{' '.join(history)}\t{' '.join(impression)}"
+        ranked_ids = request.app.state.recommender.predict(behavior)
+        articles = sorted(
+            articles, key=lambda article: ranked_ids.index(str(article.article_id))
+        )
+
+    return {
+        "status": "success",
+        "totalResults": len(articles),
+        "articles": articles,
+    }
 
 
 def verify_keyphrase(keyphrase: str = Header(...)):
