38375df6dfcf4a64ba2f7c0c1df256d2b52d1462
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-02-11 02:15:51 +0800

    Refactor API endpoints and add file upload functionality

--- app/api/base.py
+++ app/api/base.py
@@ -1,8 +1,6 @@
 from app.utils.scrapers.proxy import ProxyScraper
-from app.utils.scrapers.news import NewsScraper, Provider, get_scraper_strategy
-from app.database.database_utils import get_articles
-from fastapi import APIRouter, HTTPException
-from fastapi.responses import FileResponse, RedirectResponse
+from fastapi import APIRouter, HTTPException, UploadFile, File, Header, Depends
+from fastapi.responses import FileResponse
 import app.backend.config as config
 import os
 import datetime
@@ -31,9 +29,14 @@ def get_proxies() -> dict[str, list[str]]:
     return {"proxies": ProxyScraper().get_proxies()}
 
 
-# Refactor this endpoint
-@router.get("/download-db")
-async def download_db():
+def verify_keyphrase(keyphrase: str = Header(...)):
+    if keyphrase != os.getenv("SECRET_KEY"):
+        raise HTTPException(status_code=403, detail="Invalid keyphrase")
+    return keyphrase
+
+
+@router.get("/download-db", include_in_schema=False)
+async def download_db(keyphrase: str = Depends(verify_keyphrase)):
     db_path = os.path.join(config.get_project_root(), os.getenv("DB_NAME"))
     if not os.path.exists(db_path):
         raise HTTPException(status_code=404, detail="Database not found")
@@ -41,3 +44,17 @@ async def download_db():
     return FileResponse(
         db_path, media_type="application/octet-stream", filename=os.getenv("DB_NAME")
     )
+
+
+@router.post("/upload-db", include_in_schema=False)
+async def create_upload_db(
+    file: UploadFile = File(...), keyphrase: str = Depends(verify_keyphrase)
+):
+    if file.filename == os.getenv("DB_NAME"):
+        raise HTTPException(
+            status_code=400, detail="Cannot upload database with same name"
+        )
+    filepath = os.path.join(config.get_project_root, file.filename)
+    with open(filepath, "wb") as f:
+        f.write(file.file.read())
+    return {"message": "Database uploaded successfully"}
