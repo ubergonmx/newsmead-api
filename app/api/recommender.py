021ba13539cc191c059eb2b6fdae3f030e18061a
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-02-11 02:13:21 +0800

    Remove unnecessary code and improve error handling

--- app/api/recommender.py
+++ app/api/recommender.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 router = APIRouter()
 
 
-@router.get("/refresh-news/")
+@router.get("/refresh-news")
 async def refresh_news(request: Request, db: AsyncDatabase = Depends(get_db)):
     try:
         await request.app.state.recommender.save_news(db)
@@ -31,49 +31,29 @@ async def recommended_articles(
     log.info(f"history: {history}")
     log.info(f"articles count: {len(articles)}")
 
-    # try:
-    if history:
-        impression_news = " ".join(
-            [
-                f"{article.article_id}-{'1' if str(article.article_id) in history else '0'}"
-                for article in articles
-            ]
-        )
-        history = " ".join(history)
-        log.info(f"impression_news: {impression_news}")
-        time_now = datetime.now(timezone("Asia/Manila"))
-        behavior = f"{user_id}\t{time_now}\t{history}\t{impression_news}"
-        # await db.insert_behavior(user_id, time_now, history, impression_news)
-        ranked_ids, score = request.app.state.recommender.predict(behavior)
-        log.info(f"ranked_ids: {ranked_ids}")
-        articles = sorted(
-            articles, key=lambda article: ranked_ids.index(str(article.article_id))
-        )
-    # except Exception as e:
-    #     log.error(f"Error predicting (L{e.__traceback__.tb_lineno}): {e}")
+    try:
+        if history:
+            impression_news = " ".join(
+                [
+                    f"{article.article_id}-{'1' if str(article.article_id) in history else '0'}"
+                    for article in articles
+                ]
+            )
+            history = " ".join(history)
+            log.info(f"impression_news: {impression_news}")
+            time_now = datetime.now(timezone("Asia/Manila"))
+            behavior = f"{user_id}\t{time_now}\t{history}\t{impression_news}"
+            ranked_ids, score = request.app.state.recommender.predict(behavior)
+            await db.insert_behavior(user_id, time_now, history, impression_news, score)
+            log.info(f"ranked_ids: {ranked_ids}")
+            articles = sorted(
+                articles, key=lambda article: ranked_ids.index(str(article.article_id))
+            )
+    except Exception as e:
+        log.error(f"Error predicting (L{e.__traceback__.tb_lineno}): {e}")
 
     return {
         "status": "success",
         "totalResults": len(articles),
         "articles": articles,
     }
-
-
-def verify_keyphrase(keyphrase: str = Header(...)):
-    if keyphrase != os.getenv("SECRET_KEY"):
-        raise HTTPException(status_code=403, detail="Invalid keyphrase")
-    return keyphrase
-
-
-@router.post("/upload-db/")
-async def create_upload_db(
-    file: UploadFile = File(...), keyphrase: str = Depends(verify_keyphrase)
-):
-    if file.filename == os.getenv("DB_NAME"):
-        raise HTTPException(
-            status_code=400, detail="Cannot upload database with same name"
-        )
-    filepath = os.path.join(config.get_project_root, file.filename)
-    with open(filepath, "wb") as f:
-        f.write(file.file.read())
-    return {"message": "Database uploaded successfully"}
