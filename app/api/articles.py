1708351047cb46767ebe8d80f6fd98695eab6c33
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-03-04 14:48:59 +0800

    Translate article body to Tagalog and handle line breaks properly

--- app/api/articles.py
+++ app/api/articles.py
@@ -75,9 +75,11 @@ async def get_article(article_id: int, db: AsyncDatabase = Depends(get_db)):
         translated_title = ts.translate_text(
             article.title, translator="google", to_language="tl"
         )
-        translated_body = ts.translate_text(
-            article.body, translator="google", to_language="tl"
-        )
+        html_body = article.body.replace("\n", "<br>")
+        translated_body = ts.translate_html(
+            html_body, translator="google", to_language="tl"
+        ).replace("<br>", "\n")
+
         return {
             "title": translated_title,
             "body": translated_body,
